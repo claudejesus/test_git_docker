ca4aa21773e9665778b24a0839ac926cc4b4bd50
claudejesus <niyigabaclaujesus.com>
2025-01-23 09:06:36 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DEVOPS TASK3</title>
+</head>
+<body>
+    <a href="registration.php">REGISTRATION FORM</a>
+    <H1>WELCAME TO NEW STAFF MIS</H1>
+</body>
+</html>
