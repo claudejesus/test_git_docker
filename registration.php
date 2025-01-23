fc880c27b395096f0a9c80208f7e61ed21baac9e
claudejesus <niyigabaclaujesus.com>
2025-01-23 09:13:41 +0200

    employed,the form is added successfull

---
+++ registration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RP MUSANZE COLLEGE</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="names">
+        <input type="text" name="email">
+        <input type="text" name="password">
+        <button type="button">submitt</button>
+    </form>
+</body>
+</html>
