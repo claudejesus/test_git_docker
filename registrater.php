a9da0f690fca38628aaea0e4506d63a7dcd2872b
claudejesus <niyigabaclaujesus.com>
2025-01-23 09:57:32 +0200

    bootstrap added

---
+++ registrater.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevOps Task 3</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+    <div class="container text-center mt-5">
+        <a href="registration.php" class="btn btn-primary mb-4">Go to Registration Form</a>
+        <h1 class="text-uppercase text-success">Welcome to New Staff MIS</h1>
+    </div>
+
+    <!-- Bootstrap JS Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
